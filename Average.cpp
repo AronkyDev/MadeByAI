c8b391d3ea68065fd2ca4346eaca0ddf43595539
DFM - Marco <85831311+AronkyDev@users.noreply.github.com>
2022-06-28 17:29:01 +0200

    Update Average.cpp

--- Average.cpp
+++ Average.cpp
@@ -7,9 +7,9 @@ using namespace std;
 
 int main(){               // You have to create the main function
     int a, b, c;
-    cout << "Inserisci i tre valori: ";
+    cout << "Enter the three values: ";
     cin >> a >> b >> c;
     int media = (a + b + c) / 3;
-    cout << "La media e' " << media << endl;
+    cout << "The average is: " << media << endl;
     system("PAUSE"); // A little problem, he will replace the system("PAUSE") with the Return 0, causing the script to terminate at the end of execution.
 }
